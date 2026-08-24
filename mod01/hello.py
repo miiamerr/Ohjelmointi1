a3c32393139d72443b1cf99ce6f88876f8126caa
Miia Merranmaa <miiamerr@metropolia.fi>
2026-08-24 10:37:51 +0300

    Projektin aloitus

---
+++ mod01/hello.py
@@ -0,0 +1,5 @@
+# tämä on ensimmäinen koodini
+print("Hei maailma nimeni on Miia!")
+print("Kiva nähdä")
+name = input ("Mikä sinun nimesi on?:")
+print("Terve", name)
---
+++ mod01/readme.md
@@ -0,0 +1,5 @@
+# Ohjelmisto 1 - Python harjoitukset
+** Miia Merranmaa**
+## Moduuli 1
+Tein tehtävät 1 ja 2.
+## Moduuli 2
