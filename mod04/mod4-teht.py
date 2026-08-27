d882a2395022b6b795b0b47612324d012fdff117
Miia Merranmaa <miiamerr@metropolia.fi>
2026-08-27 09:12:37 +0300

    Initial commit

--- mod04/mod4-teht.py
+++ mod04/mod4-teht.py
@@ -15,10 +15,55 @@ elif kala >= 37:
 
 # tehtävä 2
 
-hyttiluokka = int(input("Syötä laivan hyttiluokkasi:"))
+hyttiluokka = input("Anna laivan hyttiluokkasi (LUX, A, B, C):")
 
+if hyttiluokka == "LUX":
+    print( "LUX on parvekkeellinen hyhtti yläkannella.")
+
+elif hyttiluokka == "A":
+    print ("A on ikkunallinen hytti yläkannella.")
+
+elif hyttiluokka == "B":
+    print ("B on ikkunaton hytti autokannen alapuolella.")
+
+elif hyttiluokka == "C":
+    print ("C on ikkunaton hytti autokannen alapuolella.")
+
+else:
+    print ("Virheellinen hyttiluokka.")
 
 
 # tehtävä 3
 
+sukupuoli = input("Anna biologinen sukupuolesi (mies, nainen):")
+
+hemoglobiiniarvo = int(input("Anna hemoglobiiniarvo g/l:"))
+
+if sukupuoli == "nainen":
+    if hemoglobiiniarvo < 117:
+        print("Hemoglobiiniarvo on alhainen.")
+    elif hemoglobiiniarvo <= 175:
+        print("Hemoglobiiniarvo on normaali.")
+    else:
+        print("Hemoglobiiniarvio on korkea.")
+
+if sukupuoli == "mies":
+    if hemoglobiiniarvo <134:
+        print("Hemoglobiiniarvo on alhainen.")
+    elif hemoglobiiniarvo <= 195:
+        print("Hemoglobiiniarvo on normaali.")
+    else:
+        print("Hemoglobiiniarvo on korkea.")
+
+
 # tehtävä 4
+
+vuosi = int(input("Anna vuosiluku:"))
+if vuosi % 400 == "karkausvuosi":
+    print("Vuosi on karkausvuosi.")
+elif vuosi % 100 == 0:
+    print("Vuosi ei ole karkausvuosi.")
+elif vuosi % 4 == 0:
+    print ("Vuosi on karkausvuosi.")
+else:
+    print("Vuosi ei ole karkausvuosi.")
--- readme.md
+++ readme.md
@@ -5,4 +5,4 @@ Tein tehtävät 1 ja 2.
 ## Moduuli 3
 Tein tehtävät 1, 2, 3, 4, 5 ja 6. 
 ## Moduuli 4
-Tein tehtävät 1
+Tein tehtävät 1, 2, 3 ja 4. 
